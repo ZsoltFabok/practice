af1feee3c4bf5e7d446d2e0f370ac251488e6acc
Zsolt Fabok <me@zsoltfabok.com>
2016-05-31 12:44:36 +0200

    restructure and add roman numerals

---
+++ roman-numerals-java/src/main/java/RomanNumerals.java
@@ -0,0 +1,11 @@
+public class RomanNumerals {
+
+    public static void main(String... args) {
+        RomanNumerals romanNumerals = new RomanNumerals();
+        System.out.println(romanNumerals.get());
+    }
+
+    String get() {
+        return "1";
+    }
+}
---
+++ roman-numerals-java/src/test/java/RomanNumeralsTest.java
@@ -0,0 +1,11 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class RomanNumeralsTest {
+
+    @Test
+    public void foo() {
+        assertEquals("2", new RomanNumerals().get());
+    }
+}
