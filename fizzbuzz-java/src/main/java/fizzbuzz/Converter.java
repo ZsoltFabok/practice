f902afd069f7581ab9890d7530ccde3ce80a660a
Zsolt Fabok <me@zsoltfabok.com>
2016-06-15 10:29:58 +0200

    [fizzbuzz] fix sonar issues

--- fizzbuzz-java/src/main/java/fizzbuzz/Converter.java
+++ fizzbuzz-java/src/main/java/fizzbuzz/Converter.java
@@ -6,10 +6,7 @@ import java.util.List;
  * with java 8 we won't need this class at all, <code>String.join(" ", list)</code> would do the trick.
  */
 public class Converter {
-    /**
-     * 'list_to_string' is an better read than 'listToString'.
-     */
-    public String list_to_string(List<? extends Object> list) {
+    public String listToString(List<? extends Object> list) {
         return list.toString().substring(1, list.toString().length() - 1).replaceAll(", ", " ");
     }
 }
